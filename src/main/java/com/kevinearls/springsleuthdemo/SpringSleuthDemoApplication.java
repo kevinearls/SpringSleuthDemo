541c3332a007e11d6bc27601902b2a892059e1e8
Kevin Earls <kevin@kevinearls.com>
2017-09-14 15:06:36 +0200

    Add rest template and nested call

--- src/main/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplication.java
+++ src/main/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplication.java
@@ -1,9 +1,12 @@
 package com.kevinearls.springsleuthdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.logging.Level;
@@ -14,12 +17,25 @@ import java.util.logging.Logger;
 public class SpringSleuthDemoApplication {
 
 	private static final Logger LOG = Logger.getLogger(SpringSleuthDemoApplication.class.getName());
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	@RequestMapping("/")
 	public String home() {
 		Date now = new Date();
 		LOG.log(Level.INFO, "you called home at " + now);
 		return "Hello World at " + now;
+	}
+
+	@RequestMapping("/callHome")
+	public String callHome() {
+		LOG.log(Level.INFO, "calling home");
+		return restTemplate.getForObject("http://localhost:8080", String.class);
 	}
 
 
