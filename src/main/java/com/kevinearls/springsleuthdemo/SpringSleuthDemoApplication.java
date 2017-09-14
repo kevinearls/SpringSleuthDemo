abf4c839ad325a0029fd1818552f97671972743f
Kevin Earls <kevin@kevinearls.com>
2017-09-14 14:53:51 +0200

    Add initial endpoint and turn on logging

--- src/main/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplication.java
+++ src/main/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplication.java
@@ -2,9 +2,26 @@ package com.kevinearls.springsleuthdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
 @SpringBootApplication
 public class SpringSleuthDemoApplication {
+
+	private static final Logger LOG = Logger.getLogger(SpringSleuthDemoApplication.class.getName());
+
+	@RequestMapping("/")
+	public String home() {
+		Date now = new Date();
+		LOG.log(Level.INFO, "you called home at " + now);
+		return "Hello World at " + now;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSleuthDemoApplication.class, args);
