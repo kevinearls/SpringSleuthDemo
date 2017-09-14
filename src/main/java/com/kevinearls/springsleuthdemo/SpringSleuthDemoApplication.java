43159d6188229e58d3261bc4416be5c35ae2ee77
Kevin Earls <kevin@kevinearls.com>
2017-09-14 14:40:24 +0200

    Generated code

---
+++ src/main/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplication.java
@@ -0,0 +1,12 @@
+package com.kevinearls.springsleuthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSleuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSleuthDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kevinearls/springsleuthdemo/SpringSleuthDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevinearls.springsleuthdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSleuthDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
